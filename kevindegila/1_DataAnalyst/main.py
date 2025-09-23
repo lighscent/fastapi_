4c96955cae8eeeed78e1b6d0373299e0367e376f
GrCOTE7 <grcote7@gmail.com>
2025-09-23 19:03:58 +0200

    groupby()

--- kevindegila/1_DataAnalyst/main.py
+++ kevindegila/1_DataAnalyst/main.py
@@ -4,15 +4,23 @@ import seaborn as sns
 import numpy as np
 import os
 
+
+def sl():
+    w = 99
+    print(" ─" * (w // 2) + "→")
+
+
 if __name__ == "__main__":
-    w = 178
-    print("-→" * (w // 2))
-    
+
+    sl()
+
     # //2do simplifier path
-    chemin = "D:\\fastapi\\kevindegila\\1_DataAnalyst\\datasets\\COVID-19-geographic-disbtribution-worldwide-2020-12-14.xls"
-    if os.path.exists(chemin):
-        df = pd.read_excel(chemin)
-        print(df)
-        print(df.shape)
+    pathFile = "D:\\fastapi\\kevindegila\\1_DataAnalyst\\datasets\\COVID-19-geographic-disbtribution-worldwide-2020-12-14.xls"
+    if os.path.exists(pathFile):
+        df = pd.read_excel(pathFile)
     else:
-        print("Fichier introuvable :", chemin)
+        print("Fichier introuvable :", pathFile)
+        exit()
+
+    print(df)
+    sl()
