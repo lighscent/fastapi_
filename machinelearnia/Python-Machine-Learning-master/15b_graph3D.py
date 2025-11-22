68a42a534c0da8c7c089b0f19df247aff300c52a
GrCOTE7 <grcote7@gmail.com>
2025-11-22 10:37:57 +0100

    add tk for 3d

---
+++ machinelearnia/Python-Machine-Learning-master/15b_graph3D.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+# Nécessite pip install tk
+
+iris = load_iris()
+x = iris.data
+y = iris.target
+names = list(iris.target_names)
+
+print(f"x contient {x.shape[0]} exmples et {x.shape[1]} variables")
+print(f"il y a {np.unique(y).size} classes")
+y.shape, [str(n) for n in names], int(y.max())
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
+ax.scatter(x[:, 0], x[:, 1], x[:, 2], c=y)
+plt.show()
