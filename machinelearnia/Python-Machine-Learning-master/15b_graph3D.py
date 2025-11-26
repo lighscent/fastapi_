dcb08f27d5233d0b43eace4b9138b473c05a43ba
GrCOTE7 <grcote7@gmail.com>
2025-11-26 18:13:44 +0100

    Minor corr

--- machinelearnia/Python-Machine-Learning-master/15b_graph3D.py
+++ machinelearnia/Python-Machine-Learning-master/15b_graph3D.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_iris
+
 # Nécessite pip install tk
 
 iris = load_iris()
@@ -8,7 +9,7 @@ x = iris.data
 y = iris.target
 names = list(iris.target_names)
 
-print(f"x contient {x.shape[0]} exmples et {x.shape[1]} variables")
+print(f"x contient {x.shape[0]} exemples et {x.shape[1]} variables")
 print(f"il y a {np.unique(y).size} classes")
 y.shape, [str(n) for n in names], int(y.max())
 
