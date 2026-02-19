5ab6fa6b6c6e28166243e907f7989bd7d4695089
GrCOTE7 <grcote7@gmail.com>
2026-02-19 18:02:21 +0100

    Nett

--- djangoTuto/Pyshop/Pyshop/urls.py
+++ djangoTuto/Pyshop/Pyshop/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-# from .views import animated_favicon, favicon, robots_txt
 from .views import animated_favicon, favicon, robots_txt
 
 urlpatterns = [
