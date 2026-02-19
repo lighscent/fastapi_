bc30e2ceda1019808bebe711fbbe01cba230ac5f
GrCOTE7 <grcote7@gmail.com>
2026-02-19 07:08:35 +0100

    Abstraction

--- divers/bases/abstraction.py
+++ divers/bases/abstraction.py
@@ -1,1 +1,31 @@
-print('Ok')
+from tools import *
+import flet
+from abc import ABC, abstractmethod
+
+
+class Computer(ABC):
+    """Abstract class car contient au moins une abstract method"""
+
+    @abstractmethod
+    def process(self):
+        pass
+
+
+class Laptop(Computer):
+    def process(self):
+        print("Laptop is running...")
+        # pass
+
+
+class Programmer:
+    def work(self, tool):
+        print("Solving problems by programming...")
+        tool.process()
+
+
+if __name__ == "__main__":
+    cls()
+    mc = Laptop()
+    lio = Programmer()
+    lio.work(mc)
+    sl()
